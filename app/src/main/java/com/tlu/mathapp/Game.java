6ad50af23a1c98133853055a51b7e4fb09774f36
Gertin Pakkonen <gertinnn@tlu.ee>
2020-03-19 19:04:37 +0200

    Java rakendus

---
+++ app/src/main/java/com/tlu/mathapp/Game.java
@@ -0,0 +1,127 @@
+package com.tlu.mathapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Game extends AppCompatActivity {
+    private int gameLevel = 7; // See, mitmendast levelist alustad, hetkel LVL 3. Kui on 3, on kaks arvu, kui on 5 - 3 arvu, 7 - 4 arvu.
+    private String calAnswer; // Õige vastus
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button genBtn1, genBtn2, genBtn3;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game); // Sätestab layouti
+        newRandom(); // Alguses lisab juba ühe värgi
+        genBtn1 = (Button) findViewById(R.id.answer1);
+        genBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer();
+            }
+        });
+        genBtn2 = (Button) findViewById(R.id.answer2);
+        genBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wrongPress();
+            }
+        });
+        genBtn3 = (Button) findViewById(R.id.answer3);
+        genBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wrongPress();
+            }
+        });
+    }
+    private void wrongPress(){
+        this.setCalcText("VALE! Proovi uuesti!");
+    }
+    private void checkAnswer(){
+        setCalcText("Sa oled tubli!");
+        newRandom();
+    }
+    private void newRandom() { // funktsioon, mis käivitub pärast vajutamist
+        String[] calculation = this.returnArray(gameLevel); // Tekitab array gamelevel kohaga. See vajalik pärast uute tasemete lisamiseks. Gamelevel on 3, siis tagastatakse kolmekohaline array
+        //double calAnswer = this.getCorrectAnswer(calculation); // Arvutab välja õige väärtuse
+        StringBuilder sum = new StringBuilder(); // Siia liidetakse stringidena arvud ja märgid
+        for (String calc: calculation) {
+            sum.append(calc);
+        }
+        calAnswer = getCorrectAnswer(sum.toString());
+        String calAnswerWrong1 = getWrongAnswer(calAnswer); // Arvutab randomilt õigele vastusele midagi juurde või maha, et tulemused oleks suht samas kandis, et oleks raskem pakkuda huupi
+        String calAnswerWrong2 = getWrongAnswer(calAnswer);
+        boolean done = false; // while tsükkli jaoks
+        while (!done) {
+            if((calAnswerWrong1.equals(calAnswerWrong2)) || (calAnswerWrong1.equals(calAnswer)) || (calAnswerWrong2.equals(calAnswer))){ // Tingimus, et ükski arv ei ühtiks üksteisega, tuleks võibolla lisada veel infinity jne ka.
+                calAnswerWrong1 = getWrongAnswer(calAnswer);
+                calAnswerWrong2 = getWrongAnswer(calAnswer);
+            } else {
+                done = true;
+            }
+        }
+        this.setCalcText(sum.toString());
+        this.setButtonsText(calAnswer, calAnswerWrong1, calAnswerWrong2);
+    }
+
+    private int randomNumber(double max)
+    {
+        double rand = (int)(Math.random()*((max-1)+1))+1;
+        return (int)rand;
+    }
+    private String[] returnArray(int nr){
+        String[] arr = new String[nr];
+        for (int i = 0; i < nr; i++) {
+            if((i % 2) == 0){ // paarisarv kohtadele lisan mingi arvu vahemikus 1-10
+                arr[i] = String.valueOf(randomNumber(10)); //suvalise arvu random
+            } else { // paarituarvu kohale arrays lisan tehtemärgi numbri(1 - + , 2 - - , 3 - * )
+                int rand = randomNumber(3);//tehtemärgi random
+                if(rand == 1){arr[i] = "+";}
+                else if(rand == 2){arr[i] = "-";}
+                else if(rand == 3){arr[i] = "*";}
+            }
+        }
+        return arr;
+    }
+    private String getCorrectAnswer(String ex) {
+        Object result = new Object();
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        try {
+            result = engine.eval(ex);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+    private String getWrongAnswer(String correctAnswer) {
+        double sum = 0;
+        int random = this.randomNumber(3);
+        if(random == 1){sum = Double.parseDouble(correctAnswer) + randomNumber(10);}
+        else if(random == 2){sum = Double.parseDouble(correctAnswer) - randomNumber(10);}
+        else if(random == 3){sum = Double.parseDouble(correctAnswer) * randomNumber(10);}
+        return String.valueOf(sum);
+    }
+
+    //Getters & Setters
+    private void setCalcText(String s){
+        final TextView calc = (TextView)findViewById(R.id.calc);
+        calc.setText(s);
+    }
+    private void setButtonsText(String button1, String button2, String button3){
+        final TextView answer1 = (TextView)findViewById(R.id.answer1);
+        answer1.setText(button1);
+        final TextView answer2 = (TextView)findViewById(R.id.answer2);
+        answer2.setText(button2);
+        final TextView answer3 = (TextView)findViewById(R.id.answer3);
+        answer3.setText(button3);
+    }
+}
---
+++ app/src/main/java/com/tlu/mathapp/LandingPageActivity.java
@@ -0,0 +1,31 @@
+package com.tlu.mathapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingPageActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button gameBtn;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+        gameBtn = (Button) findViewById(R.id.play_button);
+        gameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+
+    public void startGame() {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+}
