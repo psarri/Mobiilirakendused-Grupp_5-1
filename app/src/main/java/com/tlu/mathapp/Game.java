82f823cda1cf28d77699bb46ceacb5c7faf53c83
Sanderzhanni <hannis@tlu.ee>
2020-03-21 20:40:13 +0200

    task3: all functions implemented

--- app/src/main/java/com/tlu/mathapp/Game.java
+++ app/src/main/java/com/tlu/mathapp/Game.java
@@ -7,10 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
@@ -23,14 +23,21 @@ public class Game extends AppCompatActivity {
     private int gameLevel = 3; // See, mitmendast levelist alustad, hetkel LVL 3. Kui on 3, on kaks arvu, kui on 5 - 3 arvu, 7 - 4 arvu.
     private String calAnswer; // Õige vastus
     private int wrongAnswers;
+    private int score;
+    private long timeLeft;
+    //Creates a timer
     final CountDownTimer timer = new CountDownTimer(5000, 1000) {
 
+        //Calls every tick
         public void onTick(long millisUntilFinished) {
             setTimerText(Long.toString(millisUntilFinished / 1000));
+            timeLeft = millisUntilFinished / 1000;
         }
+        //Calls on finish
         public void onFinish() {
             newRandom();
-            updateGameStatus();
+            updateLives();
+            setStatusText("Out of time");
         }
     };
 
@@ -41,6 +48,7 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game); // Sätestab layouti
         newRandom(); // Alguses lisab juba ühe värgi
+
         genBtn1 = (Button) findViewById(R.id.answer1);
         genBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,13 +80,25 @@ public class Game extends AppCompatActivity {
         String quessedAnswer = getButtonText(btnId);
         if(calAnswer.equals(quessedAnswer)){
             newRandom();
+            setStatusText("correct answer");
+            calculateScore();
         } else {
-            updateGameStatus();
+            updateLives();
+            setStatusText("wrong answer");
         }
     }
 
-    private void updateGameStatus(){
+    //We only calculate score on correct answers
+    private void calculateScore(){
+        score += (gameLevel * 10) + timeLeft;
+        setScoreText(Integer.toString(score));
+    }
+
+    private void updateLives(){
         wrongAnswers += 1;
+        int lives = 5 - wrongAnswers;
+        setLivesText(Integer.toString(lives) +"/5");
+        //if user has input 5 wrong answers, redirect to game over page
         if(wrongAnswers >= 5){
             timer.cancel();
             Intent intent = new Intent(this, GameOver.class);
@@ -160,6 +180,28 @@ public class Game extends AppCompatActivity {
     private void setTimerText(String s){
         final TextView timerDisplay = (TextView) findViewById(R.id.timer);
         timerDisplay.setText(s);
+    }
+
+    private void setLivesText(String s){
+        final TextView timerDisplay = (TextView) findViewById(R.id.lives);
+        timerDisplay.setText(s);
+    }
+
+    private void setScoreText(String s){
+        final TextView timerDisplay = (TextView) findViewById(R.id.score);
+        timerDisplay.setText("Score: " + s);
+    }
+
+    private void setStatusText(String s){
+        final TextView statusMessage = (TextView) findViewById(R.id.statusMessage);
+        statusMessage.setText(s);
+        //Show the status text only for 1 second
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                statusMessage.setText("");
+            }
+        }, 1000);
     }
     private void setButtonsText(String button1, String button2, String button3){
         final TextView answer1 = (TextView)findViewById(R.id.answer1);
